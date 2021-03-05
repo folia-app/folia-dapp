783a37d93ffa832cacf5085ff0566a9d49013a7c
evvvritt <evv.info@gmail.com>
2021-03-05 03:46:52 +0100

    002 year 2021

--- lambda/works/2000000.js
+++ lambda/works/2000000.js
@@ -2,7 +2,7 @@ export default {
   titlePattern: 'Travess Smalley, "Emoji Script" {{no}}',
   editions: 500,
   generative: true,
-  description: 'PNG, 2550x2550 pixels, Variable Edition of 500, Script Created 2020',
+  description: 'PNG, 2550x2550 pixels, Variable Edition of 500, Script Created 2021',
   release: '03-05-2021 09:00:00 pm +01:00',
   directory: 'https://folia.app/works/2',
   assetPath: 'https://res.cloudinary.com/folia/image/upload/v1614897972/FLA2000000/',
