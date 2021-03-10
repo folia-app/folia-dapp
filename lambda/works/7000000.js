a9d90a7294bec125603c8800266b6c51360d0a65
evvvritt <evv.info@gmail.com>
2021-03-10 20:11:22 +0100

    metadata - add laric (7)

---
+++ lambda/works/7000000.js
@@ -0,0 +1,26 @@
+const asset = {
+  image: 'https://res.cloudinary.com/folia/image/upload/v1615403379/oliver-laric-2000-cliparts/2000-cliparts-frame1_fpxu5g.png',
+  animation_url: 'https://gateway.pinata.cloud/ipfs/QmcUtS6h1k2kNBhGrUZqyZGJStSY5vTXn5LPFLoBFtRXar/FLA7000000__oliver-laric__2000-cliparts__2010/2000%20Cliparts.mp4',
+  animation_url_optim: 'https://res.cloudinary.com/folia/video/upload/v1615403140/oliver-laric-2000-cliparts/2000-Cliparts__optim_ib7l1o.mp4'
+}
+
+export default {
+  titlePattern: 'Oliver Laric, "2000 Cliparts", 2010 {{no}}',
+  editions: 5,
+  description: 'Oliver Laric (b. 1981), "2000 Cliparts", Video/MP4, 2:54, 2000 frames, 1440x1080 pixels, 91.7MB, Edition 5 + 2AP, Created 2010, Minted 2021.',
+  release: null,
+  directory: 'https://gateway.pinata.cloud/ipfs/QmcUtS6h1k2kNBhGrUZqyZGJStSY5vTXn5LPFLoBFtRXar/FLA7000000__oliver-laric__2000-cliparts__2010',
+  assetPath: '',
+  sha256: {
+    '2000 Cliparts.mp4': '29ed225866542050c2dc75c98f3bc6dd5440fac165529133ef0f42f03b58b930'
+  },
+  tokens: {
+    7000001: asset,
+    7000002: asset,
+    7000003: asset,
+    7000004: asset,
+    7000005: asset,
+    7000006: asset,
+    7000007: asset
+  }
+}
