428c1383e53b8a1e6978ad05c5d0001af787fba8
evvvritt <evv.info@gmail.com>
2021-03-23 21:24:02 +0100

    work info - fix for petra

--- src/views/work/WorkInfo.vue
+++ src/views/work/WorkInfo.vue
@@ -2,15 +2,22 @@
   section.work-info(style="padding-bottom:25vh", v-if="doc")
     h3.sr-only Info
     //- (media)
-    figure.mb-12(v-if="!isVariableEdition")
-      router-link.block.pb-ar-1x1.relative(:to="{name: 'view-token', params: { token: Number(doc.uid) * 1000000 + 1 }}")
+
+    //- (teaser video - non-release + generatative)
+    figure.mb-12(v-if="isReleased === false && isVariableEdition")
+      video.w-full.block(:src="video", loop, muted, autoplay, playsinline)
+
+    //- singular works
+    figure.mb-12(v-else-if="!isVariableEdition")
+      router-link.block(:to="{name: 'view-token', params: { token: Number(doc.uid) * 1000000 + 1 }}")
         //- (gif)
-        template(v-if="doc.data.teaser_image.url.includes('.gif')")
+        .pb-ar-1x1.relative(v-if="doc.data.teaser_image.url && doc.data.teaser_image.url.includes('.gif')")
           .absolute.overlay.flex.items-center.justify-center.bg-white
             img-gif(:src="doc.data.teaser_image.url")
         //- (image)
         template(v-else)
           img.block.w-full(:src="doc.data.teaser_image.url", @contextmenu.prevent)
+
     //- info text
     rich-text.text-lg.px-10.lg_px-12.children-mt-em.lg_w-10x12(style="max-width:28em;", :field="doc.data.description")
 </template>
@@ -20,7 +27,13 @@ import RichText from '@/components/RichText'
 import ImgGif from '@/components/ImgGif'
 export default {
   name: 'WorkInfo',
-  props: ['doc', 'isVariableEdition'],
+  props: ['doc', 'isVariableEdition', 'isReleased'],
+  computed: {
+    video () {
+      const videoLink = this.doc?.data.teaser_video
+      return videoLink?.kind === 'document' && videoLink?.link_type === 'Media' && videoLink?.url
+    }
+  },
   components: { RichText, ImgGif }
 }
 </script>
