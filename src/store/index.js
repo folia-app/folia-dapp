65fe6cdbba92f24d4a60935970154b7a3826fe8b
evvvritt <evv.info@gmail.com>
2021-03-24 11:27:34 +0100

    mobile auction touchup

--- src/store/index.js
+++ src/store/index.js
@@ -59,7 +59,7 @@ export default new Vuex.Store({
       return prefix ? ('00' + id).slice(-3) // 001
         : id // 1 - for contract communication
     },
-    addrShort: () => (addr) => addr.slice(0, 6) + '...' + addr.slice(-4),
+    addrShort: () => (addr) => addr ? addr.slice(0, 6) + '...' + addr.slice(-4) : '...',
     userBalance: (state) => (addr) => web3?.eth.getBalance(addr || state.address) || 0, // wei
     contractAddr: (state) => state.foliaContract?._address,
     isSoldOut: () => (work) => {
