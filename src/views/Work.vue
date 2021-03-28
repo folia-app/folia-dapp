ab8aea83cbfa9d1ba2ef0d3fdca26788ee7d2e09
evvvritt <evv.info@gmail.com>
2021-03-28 23:18:17 +0200

    work sort > 3, cleanup

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -68,106 +68,11 @@
             button.focus_outline-none(@click="$router.replace({name: 'work-details'})")
               btn.px-8.md_px-12(theme="drkgray", :active="$route.name === 'work-details'") Details
 
-          //- (media for singular editions )
-          //- figure.bg-white.mb-12(v-if="!isVariableEdition")
-            .pb-ar-1x1.relative
-              .absolute.overlay.px-4.flex.items-center.justify-center
-                //- (metadata image)
-                template(v-if="!isVariableEdition")
-                  img.w-auto.max-w-full.mx-auto.block(:src="doc.data.teaser_image.url", style="image-rendering: crisp-edges;image-rendering: pixelated;")
-            //- (teaser)
-            //- template(v-if="!imgLoaded")
-              //- video.w-full.block(:src="doc.data.teaser_video.url", loop, playsinline, muted, autoplay)
-            //- countdown play
-            //- countdown-play-btn-overlay.text-sm.text-black-a30ff(v-if="hasCountdown", :doc="doc", :counter="false", size="small", @released="isReleased = true", :btnOverlay="true")
-            //- play icon
-            //- router-link.absolute.overlay.flex.items-center.justify-center(:to="{name: 'view', params: {work: Number(doc.uid) * 1000000}}")
-              <svg class="text-5xl lg_text-6xl xl_text-60 block" style="width:calc(59 / 38 * 1em); height: 1em" viewBox="0 0 59 38" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio>
-                <path d="M1 1.49251L57.3157 19L0.999998 36.5075L1 1.49251Z" fill="rgba(255,255,255,0.9)" />
-              </svg>
-
-          //- nav.px-8.lg_px-12.flex.justify-start.mb-12
-            button.focus_outline-none(@click="view = 'tokens'", v-if="isReleased && isVariableEdition")
-              btn.px-8.md_px-12(theme="drkgray", :active="view === 'tokens'") Tokens
-            button.focus_outline-none(@click="view = 'info'")
-              btn.px-8.md_px-12(theme="drkgray", :active="view === 'info'") Info
-            button.focus_outline-none(@click="view = 'owners'", v-if="isReleased && work && work.editions < 20")
-              btn.px-8.md_px-12(theme="drkgray", :active="view === 'owners'") Collectors
-            button.focus_outline-none(@click="view = 'details'")
-              btn.px-8.md_px-12(theme="drkgray", :active="view === 'details'") Details
-
-                //- rich-text.mt-20(style="max-width:32em", :field="doc.data.description")
-            //- .sticky.top-0.left-0.w-full
-              .absolute.top-0.right-0.w-screen.h-screen.flex.p-48
-              router-link.relative.w-full(v-if="doc.data.video.url", :to="{name: 'view', params: {work: doc.uid}}")
-                //- .relative.w-full.origin-center(:style="{transform: 'scale(' + imgScale + ')'}")
-                img.absolute.overlay.object-contain.object-center(:src="doc.data.icon.url")
-
-          //- figure.relative.w-full.sm_w-10x12.lg_pr-16
-            //- (metadata image)
-            template(v-if="isReleased && metadata && metadata.image")
-              img.w-full(:src="metadata.image", @load="imgLoaded = true")
-            //- (teaser)
-            template(v-if="!imgLoaded")
-              video.w-full.block(:src="doc.data.teaser_video.url", loop, playsinline, muted, autoplay)
-
-            //- play btn?
-            countdown-play-btn-overlay.text-sm.text-black-a30ff(:doc="doc", :counter="false", size="small", @released="isReleased = true", :btnOverlay="true")
-            //- router-link.absolute.overlay.flex.items-center.justify-center(:to="{name: 'view', params: {work: doc.uid}}")
-              <svg class="text-5xl lg_text-6xl xl_text-60 block" style="width:calc(59 / 38 * 1em); height: 1em" viewBox="0 0 59 38" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio>
-                <path d="M1 1.49251L57.3157 19L0.999998 36.5075L1 1.49251Z" fill="rgba(255,255,255,0.9)" />
-              </svg>
-
           router-view(:doc="doc", :isVariableEdition="isVariableEdition", :work="work", :isReleased="isReleased", :canBuy="canBuy", @buy="buy")
-
-          //- (info)
-          //- section(v-show="view === 'info'", style="padding-bottom:25vh")
-            h3.sr-only Info
-            //- (media)
-            figure.mb-12(v-if="!isVariableEdition")
-              router-link.block.pb-ar-1x1.relative(:to="{name: 'view-token', params: { token: Number(doc.uid) * 1000000 + 1 }}")
-                //- (gif)
-                template(v-if="doc.data.teaser_image.url.includes('.gif')")
-                  .absolute.overlay.flex.items-center.justify-center.bg-white
-                    img-gif(:src="doc.data.teaser_image.url")
-                //- (image)
-                template(v-else)
-                  img.block.w-full(:src="doc.data.teaser_image.url", @contextmenu.prevent)
-            //- info text
-            rich-text.text-lg.px-10.lg_px-12.children-mt-em.lg_w-10x12(style="max-width:28em;", :field="doc.data.description")
-
-          //- (details)
-          //- section.text-lg.px-10.lg_px-12(v-show="view === 'details'", style="padding-bottom:25vh")
-            h3.sr-only Details
-            rich-text.children-mt-em(:field="doc.data.details")
-            a.mt-6.inline-block.-ml-2(v-if="workAssetURL", :href="workAssetURL", target="_blank", rel="noopener noreferrer")
-              btn.px-12(theme="drkgray", size="small") File
-
-          //- (tokens)
-          //- section(v-if="isReleased && isVariableEdition", v-show="view === 'tokens'")
-            h3.sr-only Tokens
-            work-tokens(ref="tokens", :doc="doc", v-if="doc", @buy="buy", :canBuy="!isSoldOut(work)")
-
-          //- (collectors)
-          //- section.px-10.lg_px-12(v-if="work && view === 'owners'")
-            work-owners(:work="work")
-
-          //- (auctions)
-          //- section(v-show="view === 'auctions'")
-            ul
-              li(v-for="token in [4000001,4000002,4000003]")
-                router-link(:to="{name: 'work-auction', params: { work: $route.params.work, token: token }}") {{ token }}
-
-      //- close btn
-      //- button.md_hidden.absolute.top-0.right-0.p-10.focus_outline-none(@click="$router.push('/')", aria-lable="Close")
-        svg-x.cursor-pointer(style="width:1.8rem;height:1.8rem;stroke-width:1px")
 
       //- side column (auctions)
       transition(name="work-sidebar")
         router-view.flex-full.md_flex-1x2.md_max-w-1x2(name="sidebar", :doc="doc")
-      //- .flex-1.h-screen.overflow-y-scroll.scrollbars-hidden(v-if="$route.name === 'work-auction'")
-        .h-screen.bg-white
-        .h-screen.bg-gray-400
 
 </template>
 
