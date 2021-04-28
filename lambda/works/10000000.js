921f6c6e338647b41128d564c3412afbc3a78769
evvvritt <evv.info@gmail.com>
2021-04-28 18:06:54 +0200

    cloudflare ipfs

--- lambda/works/10000000.js
+++ lambda/works/10000000.js
@@ -9,63 +9,63 @@ export default {
   tokens: {
     10000001: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmahCyTa7JrrzwqZWi1Dk4PQ31f3FRYkLCrgjXR6qjP6cu/10000001__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmahCyTa7JrrzwqZWi1Dk4PQ31f3FRYkLCrgjXR6qjP6cu/10000001__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmahCyTa7JrrzwqZWi1Dk4PQ31f3FRYkLCrgjXR6qjP6cu/10000001__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmahCyTa7JrrzwqZWi1Dk4PQ31f3FRYkLCrgjXR6qjP6cu/10000001__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/chamber.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/1'
     },
     10000002: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmNUVTxtByUBZGBYsiDbKDFw1JMbVHLpThRXMcqJBeTbuc/10000002__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmNUVTxtByUBZGBYsiDbKDFw1JMbVHLpThRXMcqJBeTbuc/10000002__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmNUVTxtByUBZGBYsiDbKDFw1JMbVHLpThRXMcqJBeTbuc/10000002__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmNUVTxtByUBZGBYsiDbKDFw1JMbVHLpThRXMcqJBeTbuc/10000002__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/2.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/2'
     },
     10000003: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmRdnwup3tbgwYodnxN9oaHoYCRYaz4jFTyN8sZbrNRM5m/10000003__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmRdnwup3tbgwYodnxN9oaHoYCRYaz4jFTyN8sZbrNRM5m/10000003__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmRdnwup3tbgwYodnxN9oaHoYCRYaz4jFTyN8sZbrNRM5m/10000003__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmRdnwup3tbgwYodnxN9oaHoYCRYaz4jFTyN8sZbrNRM5m/10000003__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/drum%20duet%203.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/3'
     },
     10000004: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmVSXVe9jHgHCQiyxZCVxx6AucLrxjHRgSUf7Xz3NrrHLR/10000004__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmVSXVe9jHgHCQiyxZCVxx6AucLrxjHRgSUf7Xz3NrrHLR/10000004__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmVSXVe9jHgHCQiyxZCVxx6AucLrxjHRgSUf7Xz3NrrHLR/10000004__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmVSXVe9jHgHCQiyxZCVxx6AucLrxjHRgSUf7Xz3NrrHLR/10000004__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/4c.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/4'
     },
     10000005: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmP1NKmpQFDK8z483Ec73HG9piJwMRMYxMDBeEBjuJ4MvH/10000005__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmP1NKmpQFDK8z483Ec73HG9piJwMRMYxMDBeEBjuJ4MvH/10000005__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmP1NKmpQFDK8z483Ec73HG9piJwMRMYxMDBeEBjuJ4MvH/10000005__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmP1NKmpQFDK8z483Ec73HG9piJwMRMYxMDBeEBjuJ4MvH/10000005__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/5c.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/5'
     },
     10000006: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmQs7ZMa2opBdMXb4uwxZ5CSTrqrByrXSEfPmMg9ovSdpF/10000006__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmQs7ZMa2opBdMXb4uwxZ5CSTrqrByrXSEfPmMg9ovSdpF/10000006__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmQs7ZMa2opBdMXb4uwxZ5CSTrqrByrXSEfPmMg9ovSdpF/10000006__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmQs7ZMa2opBdMXb4uwxZ5CSTrqrByrXSEfPmMg9ovSdpF/10000006__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/6.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/6'
     },
     10000007: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmYoX9B7XPL2VnW6waNsUwB1vHg4MRxRixQoFDLuJA2hUy/10000007__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmYoX9B7XPL2VnW6waNsUwB1vHg4MRxRixQoFDLuJA2hUy/10000007__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmYoX9B7XPL2VnW6waNsUwB1vHg4MRxRixQoFDLuJA2hUy/10000007__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmYoX9B7XPL2VnW6waNsUwB1vHg4MRxRixQoFDLuJA2hUy/10000007__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/7d.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/7'
     },
     10000008: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmdwzR5bu15qANmjkHjvUg6Gatj4V9Eq2gKSsjc8EvihDx/10000008__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmdwzR5bu15qANmjkHjvUg6Gatj4V9Eq2gKSsjc8EvihDx/10000008__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmdwzR5bu15qANmjkHjvUg6Gatj4V9Eq2gKSsjc8EvihDx/10000008__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmdwzR5bu15qANmjkHjvUg6Gatj4V9Eq2gKSsjc8EvihDx/10000008__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/8.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/8'
     },
     10000009: {
       directory: 'https://gateway.pinata.cloud/ipfs/QmSxvZXnHroXe7QjXhSt6exJsP8Cm1ZRyVCtrHUFkVmGBB/10000009__Billy-Bultheel__Songs-for-the-Contract__2021',
-      image: 'https://gateway.pinata.cloud/ipfs/QmSxvZXnHroXe7QjXhSt6exJsP8Cm1ZRyVCtrHUFkVmGBB/10000009__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
+      image: 'https://cloudflare-ipfs.com/ipfs/QmSxvZXnHroXe7QjXhSt6exJsP8Cm1ZRyVCtrHUFkVmGBB/10000009__Billy-Bultheel__Songs-for-the-Contract__2021/orb.jpg',
       animation_url: 'https://gateway.pinata.cloud/ipfs/QmSxvZXnHroXe7QjXhSt6exJsP8Cm1ZRyVCtrHUFkVmGBB/10000009__Billy-Bultheel__Songs-for-the-Contract__2021/thumb.mp4',
       animation_thumb: 'https://folia-bultheel.fra1.cdn.digitaloceanspaces.com/orbs/9.mp4',
       iframe: 'https://songs-for-the-contract.folia.app/9'
