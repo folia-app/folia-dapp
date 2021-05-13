c41e9649657d53d0266c73854f100ee9027a45d7
evvvritt <evv.info@gmail.com>
2021-05-14 00:31:01 +0200

    segal meta with cloudinary placeholders

--- lambda/works/11000000.js
+++ lambda/works/11000000.js
@@ -2,7 +2,7 @@ export default {
   // titlePattern: 'Songs for the Contract {{no}}',
   editions: 3,
   // generative: true,
-  description: 'Emily Segal, from the series "Code of Holes", 2021, GPT3 Poems.',
+  description: 'Emily Segal, from the series "Code of Holes", 2021, GPT-3 Poem.',
   // release: '03-05-2021 09:00:00 pm +01:00',
   directory: null,
   assetPath: '',
@@ -10,26 +10,26 @@ export default {
     11000001: {
       title: 'FUTURIST',
       directory: '',
-      image: '',
-      animation_url: 'https://code-of-holes.netlify.app/futurist',
-      animation_thumb: '',
-      iframe: ''
+      image: 'https://res.cloudinary.com/folia/image/upload/v1620944844/segal-poems/FUTURIST_bc7n7c.png',
+      animation_url: 'https://res.cloudinary.com/folia/video/upload/v1620944855/segal-poems/FUTURIST_x610hx.mp4',
+      // animation_thumb: '',
+      iframe: 'https://code-of-holes.netlify.app/futurirst'
     },
     11000002: {
       title: 'PLASTICBEACH',
       directory: '',
-      image: '',
+      image: 'https://res.cloudinary.com/folia/image/upload/v1620944845/segal-poems/PLASTICBEACH_xhk7ix.png',
       animation_url: 'https://code-of-holes.netlify.app/plasticbeach',
-      animation_thumb: '',
-      iframe: ''
+      // animation_thumb: '',
+      iframe: 'https://code-of-holes.netlify.app/plasticbeach'
     },
     11000003: {
       title: 'HOLES',
       directory: '',
-      image: '',
-      animation_url: 'https://code-of-holes.netlify.app/holes',
-      animation_thumb: '',
-      iframe: ''
+      image: 'https://res.cloudinary.com/folia/image/upload/v1620944843/segal-poems/HOLES_woznsr.png',
+      animation_url: 'https://res.cloudinary.com/folia/video/upload/v1620944853/segal-poems/HOLES_fzv1wp.mp4',
+      // animation_thumb: '',
+      iframe: 'https://code-of-holes.netlify.app/holes'
     }
   }
 }
