36fcbc47dea830f52e3d09cc0e95a4f3d8e6776e
evvvritt <evv.info@gmail.com>
2021-05-15 15:04:01 +0200

    emily meta revert to pinata gateway bc opensea doesn't like cloudflare?

--- lambda/works/11000000.js
+++ lambda/works/11000000.js
@@ -4,27 +4,27 @@ export default {
   // generative: true,
   description: 'Emily Segal, 2021, GPT-3 Poem (PDF/HTML), from the series "Code of Holes" {{no}}.',
   // release: '03-05-2021 09:00:00 pm +01:00',
-  directory: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS',
+  directory: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/HOLES.png',
   assetPath: '',
   tokens: {
     11000001: {
       title: 'CODE OF HOLES',
-      image: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/HOLES.png',
-      animation_url: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/HOLES.mp4',
+      image: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/HOLES.png',
+      animation_url: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/HOLES.mp4',
       animation_thumb: 'https://res.cloudinary.com/folia/video/upload/v1620950482/segal-poems/HOLES--no-intro--720_wqjqb9.mp4',
       iframe: 'https://deluge.folia.app/holes'
     },
     11000002: {
       title: 'PLASTICBEACH',
-      image: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/PLASTICBEACH.png',
-      animation_url: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/PLASTICBEACH.mp4',
+      image: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/PLASTICBEACH.png',
+      animation_url: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/PLASTICBEACH.mp4',
       animation_thumb: 'https://res.cloudinary.com/folia/video/upload/v1620950485/segal-poems/PLASTICBEACH--no-intro--720_gocdyr.mp4',
       iframe: 'https://deluge.folia.app/plasticbeach'
     },
     11000003: {
       title: 'FUTURIST',
-      image: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/FUTURIST.png',
-      animation_url: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/FUTURIST.mp4',
+      image: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/FUTURIST.png',
+      animation_url: 'https://gateway.pinata.cloud/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021/FUTURIST.mp4',
       animation_thumb: 'https://res.cloudinary.com/folia/video/upload/v1620950481/segal-poems/FUTURIST--no-intro--720_m6ri63.mp4',
       iframe: 'https://deluge.folia.app/futurist'
     }
