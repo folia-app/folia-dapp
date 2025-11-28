c425e2453d2f343a45982269236610b39e3b7d67
evvvritt <evv.info@gmail.com>
2025-11-28 15:56:23 +0100

    string replace gateway domain

--- lambda/works/9000000.js
+++ lambda/works/9000000.js
@@ -3,7 +3,7 @@ export default {
   editions: 1,
   description: 'Petra Cortright (b. 1986), James Whipple (b. 1985), "Untitled 02", Video/MP4, 0:59, 1616 x 1080 pixels, 75.4MB, Edition of 1',
   release: null,
-  directory: 'https://gateway.pinata.cloud/ipfs/QmZMqGrLe1cNd7cAyhrPMehb3zXHCbjsW1WDMJptcvn67z/FLA9000001__Petra-Cortright-James-Whipple__Untitled-02',
+  directory: 'https://gateway.lighthouse.storage/ipfs/QmZMqGrLe1cNd7cAyhrPMehb3zXHCbjsW1WDMJptcvn67z/FLA9000001__Petra-Cortright-James-Whipple__Untitled-02',
   assetPath: '',
   sha256: {
     'FLA9000001.mp4': '08a89678ec27cb063e37b20b29ab2867fbab149191949c65d21b955651e73817'
@@ -11,9 +11,9 @@ export default {
   background: '#fefefe',
   tokens: {
     9000001: {
-      image: 'https://gateway.pinata.cloud/ipfs/QmbMyuXANJszqz7diafYT2717pVUbGmmBfmgeC9Yk9yiVB/FLA9000001-frame0.png',
-      animation_url: 'https://gateway.pinata.cloud/ipfs/QmZMqGrLe1cNd7cAyhrPMehb3zXHCbjsW1WDMJptcvn67z/FLA9000001__Petra-Cortright-James-Whipple__Untitled-02/FLA9000001.mp4',
-      animation_url_optim: 'https://gateway.pinata.cloud/ipfs/QmbMyuXANJszqz7diafYT2717pVUbGmmBfmgeC9Yk9yiVB/FLA9000001-web.mp4',
+      image: 'https://gateway.lighthouse.storage/ipfs/QmbMyuXANJszqz7diafYT2717pVUbGmmBfmgeC9Yk9yiVB/FLA9000001-frame0.png',
+      animation_url: 'https://gateway.lighthouse.storage/ipfs/QmZMqGrLe1cNd7cAyhrPMehb3zXHCbjsW1WDMJptcvn67z/FLA9000001__Petra-Cortright-James-Whipple__Untitled-02/FLA9000001.mp4',
+      animation_url_optim: 'https://gateway.lighthouse.storage/ipfs/QmbMyuXANJszqz7diafYT2717pVUbGmmBfmgeC9Yk9yiVB/FLA9000001-web.mp4',
       properties: {
         artist: 'Petra Cortright & James Whipple',
         series: 'Untitled'
